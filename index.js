5919f35ff696c9a10901d33243bb0911ce58e3aa
MattoDev <mateofonseca.profesional@gmail.com>
2024-03-13 11:46:15 -0500

    environment and public file

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 const express = require("express");
+require("dotenv").config();
+
+console.log(process.env);
 
 //Create express server
 const app = express();
 
 //Rutas
-app.get("/", (req, res) => {
+/* app.get("/", (req, res) => {
   console.log("se requiere el slash");
   res.json({
     ok: true,
   });
-});
+}); */
+
+//Public directory
+//use middleware
+app.use(express.static("public"));
 
 //Listen request
-app.listen(4000, () => {
-  console.log(`Servidor corriendo en puerto ${4000}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
 });
