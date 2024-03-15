76c1f8aaf708f91aa9d38bcc3ab8f9e401ef9f22
MattoDev <mateofonseca.profesional@gmail.com>
2024-03-14 20:08:28 -0500

    Express validator

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,5 @@
 const { Router } = require("express");
+const { check } = require("express-validator");
 const router = Router();
 const {
   createUser,
@@ -6,11 +7,31 @@ const {
   revalidateToken,
 } = require("../controllers/auth");
 
-router.post("/new", createUser);
+router.post(
+  "/new",
+  [
+    /* middlewares */
+    check("name", "El nombre es obligatorio").not().isEmpty(),
+    check("email", "El email es olbigatorio").isEmail(),
+    check("password", "el password debe de ser de 6 caracteres").isLength({
+      min: 6,
+    }),
+  ],
+  createUser
+);
 
-router.post("/", loginUser);
+router.post(
+  "/",
+  [
+    /* middlewares */
+    check("email", "El email es olbigatorio").isEmail(),
+    check("password", "el password debe de ser de 6 caracteres").isLength({
+      min: 6,
+    }),
+  ],
+  loginUser
+);
 
 router.get("/renew", revalidateToken);
 
 module.exports = router;
-
